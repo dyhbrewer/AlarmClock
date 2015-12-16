b19c9d29fa153a81c868e18b6f445d0ddd066143
dyhbrewer <xdxuhehui@gmail.com>
2015-12-16 10:47:49 +0800

    Initial Commit

---
+++ AlarmClock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlarmClock
+//
+//  Created by Xu on 15/12/16.
+//  Copyright © 2015年 Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlarmClock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlarmClock
+//
+//  Created by Xu on 15/12/16.
+//  Copyright © 2015年 Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
